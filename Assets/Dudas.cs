faa94bd8aceb6d5631852ff976c85fd4975fe201
Akuraaa <72999443+Akuraaa@users.noreply.github.com>
2020-11-05 23:59:40 -0300

    Update Dudas.cs

--- Assets/Dudas.cs
+++ Assets/Dudas.cs
@@ -2,4 +2,6 @@
  * Arreglar botones
  * Como hacer para que el usuario cree nuevos prefabs y añadirlos a la lista de enum
  * Como hacer para que el + abra la ventana, intenté castearla pero no me deja.
+ * Si no hay un asset en la escena, se debe crear uno seleccionado
+ * 
  */
