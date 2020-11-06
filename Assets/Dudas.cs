4ba71d2af10758cfb0187e6bd22abeb82cd41f15
Akuraaa <72999443+Akuraaa@users.noreply.github.com>
2020-11-05 23:54:44 -0300

    asd

---
+++ Assets/Dudas.cs
@@ -0,0 +1,5 @@
+﻿/*
+ * Arreglar botones
+ * Como hacer para que el usuario cree nuevos prefabs y añadirlos a la lista de enum
+ * Como hacer para que el + abra la ventana, intenté castearla pero no me deja.
+ */
--- Assets/Scripts/Editor/Tile/TileEditor.cs
+++ Assets/Scripts/Editor/Tile/TileEditor.cs
@@ -17,23 +17,23 @@ public class TileEditor : Editor
     {
         Handles.BeginGUI();
         CreateButton(tgt.currentTileName.ToString(), tgt.transform.position, Vector3.zero, Vector3.zero);
-         RotateButton("o", tgt.transform.position, tgt.transform);
+        RotateButton("o", tgt.transform.position, tgt.transform);
         var addValue = 250 / Vector3.Distance(Camera.current.transform.position, tgt.transform.position);
-        if (tgt.forward && (!Physics.Raycast(tgt.transform.position, tgt.transform.forward, 20, layerMask: 9)) /*Physics.Raycast(tgt.transform.position, tgt.transform.forward, )*/)
+        if (tgt.forward && (!Physics.Raycast(tgt.transform.position, tgt.transform.forward, 20, layerMask: 9)))
         {
-            CreateButton("+", tgt.transform.forward * addValue, tgt.forward.transform.forward, tgt.forward.transform.position);
+            CreateButton("+", tgt.forward.transform.position, tgt.forward.transform.forward, tgt.forward.transform.position);
         }
         //if (tgt.back && (Physics.Raycast(tgt.transform.position, -tgt.transform.forward, 20)))
         //{
         //    CreateButton("+", -tgt.transform.forward * addValue, tgt.back.transform.forward, tgt.back.transform.position);         
         //}
-        if (tgt.right && (!Physics.Raycast(tgt.transform.position, tgt.transform.right, 20, layerMask: 9)) /*Physics.Raycast(tgt.transform.position, tgt.transform.right, 5)*/)
+        if (tgt.right && (!Physics.Raycast(tgt.transform.position, tgt.transform.right, 20, layerMask: 9)))
         {
-            CreateButton("+", tgt.transform.right * addValue, tgt.right.transform.forward, tgt.right.transform.position);
+            CreateButton("+", tgt.right.transform.position, tgt.right.transform.forward, tgt.right.transform.position);
         }
-        if (tgt.left && (!Physics.Raycast(tgt.transform.position, -tgt.transform.right, 20, layerMask: 9)) /*&& Physics.Raycast(tgt.transform.position, -tgt.transform.right, 5)*/)
+        if (tgt.left && (!Physics.Raycast(tgt.transform.position, -tgt.transform.right, 20, layerMask: 9)))
         { 
-            CreateButton("+", -tgt.transform.right * addValue, tgt.left.transform.forward, tgt.left.transform.position);
+            CreateButton("+", tgt.left.transform.position, tgt.left.transform.forward, tgt.left.transform.position);
         } 
         Handles.EndGUI();
     }
@@ -42,38 +42,52 @@ public class TileEditor : Editor
     private void CreateButton(string text, Vector3 pos, Vector3 dir, Vector3 nPos)
     {
         var _pos = Camera.current.WorldToScreenPoint(pos);
-        var size = 2000 / Vector3.Distance(Camera.current.transform.position, pos);
+        var size = 1000 / Vector3.Distance(Camera.current.transform.position, pos);
         var rect = new Rect(_pos.x - size / 2, Screen.height - _pos.y - size, size, size /2);
 
         if (GUI.Button(rect, text))
         {
             Tile t = new Tile();
-            switch(PrefabWindow.prefab)
+            if (t != null)
             {
-                case "Pared":
-                    t = (Tile)Resources.Load("Prefabs/Pared", typeof(Tile));
-                    break;
-                case "Final":
-                    t = (Tile)Resources.Load("Prefabs/Final", typeof(Tile));
-                    break;
-                case "Cruce":
-                    t = (Tile)Resources.Load("Prefabs/Cruce", typeof(Tile));
-                    break;
-                case "ParedConcava":
-                    t = (Tile)Resources.Load("Prefabs/ParedConcava", typeof(Tile));
-                    break;
-                case "PisoTecho":
-                    t = (Tile)Resources.Load("Prefabs/PisoTecho", typeof(Tile));
-                    break;
-                case "PuertaAbierta":
-                    t = (Tile)Resources.Load("Prefabs/PuertaAbierta", typeof(Tile));
-                    break;
+                switch (PrefabWindow.prefab)
+                {
+                    case "Pared":
+                        t = (Tile)Resources.Load("Prefabs/Pared", typeof(Tile));
+                        break;
+                    case "Final":
+                        t = (Tile)Resources.Load("Prefabs/Final", typeof(Tile));
+                        break;
+                    case "Cruce":
+                        t = (Tile)Resources.Load("Prefabs/Cruce", typeof(Tile));
+                        break;
+                    case "ParedConcava":
+                        t = (Tile)Resources.Load("Prefabs/ParedConcava", typeof(Tile));
+                        break;
+                    case "PisoTecho":
+                        t = (Tile)Resources.Load("Prefabs/PisoTecho", typeof(Tile));
+                        break;
+                    case "PuertaAbierta":
+                        t = (Tile)Resources.Load("Prefabs/PuertaAbierta", typeof(Tile));
+                        break;
+                }
+                Tile _tile = Instantiate(t);
+                _tile.transform.forward = dir;
+                _tile.transform.position = nPos + (_tile.transform.forward.normalized * Vector3.Distance(_tile.back.transform.position, _tile.transform.position));
+                Selection.activeObject = _tile;
+                SceneView.lastActiveSceneView.LookAt(_tile.transform.position);
             }
-            Tile _tile = Instantiate(t);
-            _tile.transform.forward = dir;
-            _tile.transform.position = nPos + (_tile.transform.forward.normalized * Vector3.Distance(_tile.back.transform.position, _tile.transform.position));
-            Selection.activeObject = _tile;
-            SceneView.lastActiveSceneView.LookAt(_tile.transform.position);
+            else
+            {
+                EditorGUILayout.LabelField("Necesitas ir a LevelWindows/Prefab Editor", EditorStyles.wordWrappedLabel);
+                GUILayout.Space(20);
+                if (GUILayout.Button("Ya capté la movida pá"))
+                {
+
+                }
+
+
+            }
         }
     }
 
@@ -81,7 +95,7 @@ public class TileEditor : Editor
     {
         var _pos = Camera.current.WorldToScreenPoint(pos);
         var size = 2000 / Vector3.Distance(Camera.current.transform.position, pos);
-        var rect = new Rect(_pos.x - size, Screen.height - _pos.y - size, size/4, size / 4);
+        var rect = new Rect(_pos.x - size / 2, Screen.height - _pos.y - size, size/4, size / 4);
 
         if (GUI.Button(rect, text))
         {
