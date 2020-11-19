4c67b3d6c7aff88e2ab88db6bd02306baab0988b
AG87 <30276554+agusgoku87@users.noreply.github.com>
2020-11-19 16:06:12 -0300

    Arreglado PAAAA

--- Assets/Scripts/Editor/Tile/TileEditor.cs
+++ Assets/Scripts/Editor/Tile/TileEditor.cs
@@ -17,31 +17,31 @@ public class TileEditor : Editor
     void OnSceneGUI()
     {
         Handles.BeginGUI();
-        var addValue = 300 / Vector3.Distance(Camera.current.transform.position, tgt.transform.position);
+        //var addValue = 300 / Vector3.Distance(Camera.current.transform.position, tgt.transform.position);
 
-        var _pos = Camera.current.WorldToScreenPoint(tgt.transform.position);
-        var size = 2000 / Vector3.Distance(Camera.current.transform.position, tgt.transform.position);
-        var r = new Rect(_pos.x, Screen.height - _pos.y - 100, 45, 45);
-        GUI.Button(r, tgt.currentTileName.ToString());
+        //var _pos = Camera.current.WorldToScreenPoint(tgt.transform.position);
+        //var size = 2000 / Vector3.Distance(Camera.current.transform.position, tgt.transform.position);
+        //var r = new Rect(_pos.x, Screen.height - _pos.y - 100, 45, 45);
+        //GUI.Button(r, tgt.currentTileName.ToString());
         //CreateButton(tgt.currentTileName.ToString(), tgt.transform.position, Vector3.zero, Vector3.zero);
             RotateButton("o", tgt.transform.position, tgt.transform);
         if (tgt.forward && (!Physics.Raycast(tgt.transform.position, tgt.transform.forward, 20, layerMask: 9)))
         {
-            CreateButton("+", tgt.forward.transform.position * 1, tgt.forward.transform.forward, tgt.forward.transform.position);
+            CreateButton("+", tgt.forward.transform.position * 1, tgt.forward.transform.forward);
         }
         if (tgt.right && (!Physics.Raycast(tgt.transform.position, tgt.transform.right, 20, layerMask: 9)))
         {
-            CreateButton("+", tgt.right.transform.position * 1, tgt.right.transform.forward, tgt.right.transform.position);
+            CreateButton("+", tgt.right.transform.position * 1, tgt.right.transform.forward);
         }
         if (tgt.left && (!Physics.Raycast(tgt.transform.position, -tgt.transform.right, 20, layerMask: 9)))
         { 
-            CreateButton("+", tgt.left.transform.position *1, tgt.left.transform.forward, tgt.left.transform.position);
+            CreateButton("+", tgt.left.transform.position *1, tgt.left.transform.forward);
         } 
         Handles.EndGUI();
     }
    
 
-    private void CreateButton(string text, Vector3 pos, Vector3 dir, Vector3 nPos) //el npos es al pedo 
+    private void CreateButton(string text, Vector3 pos, Vector3 dir) //el npos es al pedo 
     {
         var _pos = Camera.current.WorldToScreenPoint(pos);
         var size = 2000 / Vector3.Distance(Camera.current.transform.position, pos);
@@ -52,11 +52,12 @@ public class TileEditor : Editor
             {
                 var t = (Tile)Resources.Load("Prefabs/" + PrefabWindow.selectedObject.name, typeof(Tile));
                 var _tile = Instantiate(t);
-                Debug.Log("Llege");
-                t.transform.forward = dir;
-                t.transform.position = nPos + (t.transform.forward.normalized * Vector3.Distance(t.back.transform.position, t.transform.position));
-                Selection.activeGameObject = t.gameObject;
-                SceneView.lastActiveSceneView.LookAt(t.transform.position);
+                _tile.transform.forward = dir;
+                Vector3 TPos = _tile.transform.forward.normalized * Vector3.Distance(_tile.back.transform.position, _tile.transform.position);
+                _tile.transform.position = pos + TPos;
+
+                Selection.activeGameObject = _tile.gameObject;
+                SceneView.lastActiveSceneView.LookAt(_tile.transform.position);
             }
             else
             {
